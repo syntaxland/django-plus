8f61140760d1e4936f4a03b24001da77f2c607f3
syntaxland <chibuzo.okenwa@email.com>
2023-03-25 15:49:43 +0100

    First commit

--- tiktok/urls.py
+++ tiktok/urls.py
@@ -13,7 +13,11 @@
 from django.contrib import admin
 from django.urls import path, include 
 from django.views.generic import TemplateView 
-from django.contrib.auth.views import LogoutView 
+from django.contrib.auth.views import LogoutView
+
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [ 
     path('admin/', admin.site.urls),
@@ -34,4 +38,6 @@ urlpatterns = [
 
     path('accounts/', include('allauth.urls')), 
     path('logout', LogoutView.as_view()),
-    ]
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
